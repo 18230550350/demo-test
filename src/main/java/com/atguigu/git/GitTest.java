e08dbbb7cb681ba09a48dca4d6e9277af7537d89
18230550350 <80400547+18230550350@users.noreply.github.com>
2021-11-28 21:43:15 +0800

    three1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("zhangsan");
+        System.out.println("李四");
     }
 }
